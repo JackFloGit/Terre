a20e74d73e31d78df838c48f3ec48de32781fbcd
JackFloGit <jack.flor@hotmail.com>
2023-03-28 19:26:44 +0200

    modif terre04

--- terre05.py
+++ terre05.py
@@ -11,3 +11,26 @@
 #
 #       --> python exo.py 3 5
 #           erreur.
+
+
+import sys
+
+if len(sys.argv[1:]) < 1:          
+ print("Bien essayé mais il faut des chiffres entiers !")   
+else :
+      if len(sys.argv[1:]) >= 1 :  # arg= 0 ou 1 
+      
+        arg = ''.join(sys.argv[1:])
+      t_f = True
+      
+      for num in arg:
+          if num < chr(48) or num > chr(57):
+              t_f = False
+      
+      if not t_f:
+          print("Bien essayer mais il faut des chiffres eniers !")
+      else :                                                        # arg integer != float or ascii.letter
+           if int(num) % 2 == 0:
+                 print("pair") 
+           if int(num) % 2 != 0 :
+             print ("impair")

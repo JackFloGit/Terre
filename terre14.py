bac6559c1c8989dd597a19d35701fe1ffca0ef5a
JackFloGit <127833907+JackFloGit@users.noreply.github.com>
2023-04-26 01:32:15 +0200

    Update terre14.py

--- terre14.py
+++ terre14.py
@@ -1,26 +1,43 @@
-#Créez un programme qui détermine si une liste d’entiers est triée ou pas.
+# Créez un programme qui détermine si une liste d’entiers est triée ou pas.
 #
 #
-#Exemples d’utilisation :
-#$> ruby exo.rb 9 8 3
-#Pas triée !
+# Exp :
+#       --> python script.py 9 8 3
+#           Pas triée !
 #
-#$> ruby exo.rb 3 8 9 12
-#Triée !
+#       --> python script.py 3 8 9 12
+#           Triée !
 #
-#$> ruby exo.rb “Salut”
-#erreur.
+#       --> python script.py “Salut”
+#           erreur.
 #
 
+import sys
 
+if len(sys.argv[1:]) < 2:
+    print("erreur")
+    exit()
 
+liste= []
 
+for arg in sys.argv[1:]:
+    if not arg.isdigit():
+        print("erreur")
+        exit()
+    intarg=int(arg)
+    liste.append(intarg)
 
+x=len(liste)
+i=0
+while i < (x-1):
+    if liste[i] < liste[((i)+1)]:
+        sort=True
+    else:
+       sort=False
 
+    i=i+1
 
-
-
-
-
-
-
+if sort == True:
+    print("Triée !")
+else:
+    print("Pas triée !")

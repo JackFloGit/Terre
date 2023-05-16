c37f01414a86618ff21a48f4fd22f1f75b177225
JackFloGit <jack.flor@hotmail.com>
2023-05-16 16:59:32 +0200

    terre05

--- terre05.py
+++ terre05.py
@@ -14,34 +14,41 @@
 
 import sys
 
-if len(sys.argv[1:]) != 2 :                                     
-     print ("il faut un dividende et un diviseur")
-     exit()
 
-for arg in sys.argv[1:]: 
-  if arg[0] == '-':
-    arg = arg[1:]
+def lenght_argument():
+    if len(sys.argv[1:]) != 2:
+        print ("il faut un dividende et un diviseur")
+        exit()
 
-isdig0=(sys.argv[1]).isdigit()
-isdig1=(sys.argv[2]).isdigit()
 
-if isdig0 is False or isdig1 is False:
-    print("Bien essayer mais il faut des chiffres entiers !")
-    exit() 
-                                         
+def int_numbers_check():
+    is_first_arg_digit=(sys.argv[1]).isdigit()
+    is_second_arg_digit=(sys.argv[2]).isdigit()
+    if not is_first_arg_digit or not is_second_arg_digit:
+        print("Bien essayer mais il faut des chiffres entiers !")
+        exit()
 
-num0=int(sys.argv[1])   #divende
-num1=int(sys.argv[2])   #diviseur 
 
-if num0 < num1:
-    print("Erreur le diviseur doit être inférieur au dividende")
-    exit() 
-if num1 == 0:
-    print ("Erreur le diviseur ne peut pas être nul")  
-    exit()
+def calculation_rules():
+    dividende = int(sys.argv[1])
+    diviseur = int(sys.argv[2])
+    if dividende < diviseur:
+        print("Erreur le diviseur doit être inférieur au dividende")
+        exit()
+    if diviseur == 0:
+        print ("Erreur le diviseur ne peut pas être nul")
+        exit()
 
-quotient = (num0) // (num1)
-print ('resultat :',quotient)
 
-reste = num0 % num1 
-print ('reste :',reste)
+def generate_result():
+    dividende = int(sys.argv[1])
+    diviseur = int(sys.argv[2])
+    quotient = (dividende) // (diviseur)
+    print ('resultat :',quotient)
+    reste = dividende % diviseur
+    print ('reste :',reste)
+
+lenght_argument()
+int_numbers_check()
+calculation_rules()
+generate_result()

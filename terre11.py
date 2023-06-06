b736972a390558f3246273a097d64419113f2401
JackFloGit <jack.flor@hotmail.com>
2023-06-06 11:42:10 +0200

    terre11

--- terre11.py
+++ terre11.py
@@ -1,76 +1,71 @@
-# Créez un programme qui transforme une heure  
-# affichée en format 24h en
-# une heure affichée en format 12h.
-#
-# exp : 
-#       --> python script.py 23:40
-#           11:40PM
-#
-# !! attention midi et minuit !!
-#
+'''Créez un programme qui transforme une heure  
+ affichée en format 24h en
+ une heure affichée en format 12h.
+
+ exp : 
+      --> python script.py 23:40
+         11:40PM
+
+ !! attention midi et minuit !!'''
+
 
 import sys
 
-# format
-if len(sys.argv[1:]) != 1:
-    print("erreur")
-    exit()
 
-found=sys.argv[1].find(":")
-if found == -1:
-    print ("erreur")
-    exit()
-
-
-for arg in sys.argv[1:]:  
- point=arg.find(":")
-
-bloc1=arg[:(point)]
-bloc2=arg[((point)+1):]
-
-if len(bloc1) != 2:
-   print("erreur")
-   exit()
-
-
-if len(bloc2) != 2:
-   print("erreur")
-   exit()
-
-#chiffre
-isdig1=bloc1.isdigit()
-isdig2=bloc2.isdigit()
-
-if isdig1 != True or isdig2 != True:
-    print("erreur")
-    exit()
-
-# horaire
-heure=int(bloc1)
-minute=int(bloc2)
-
-
-if heure < 0 or heure > 23:
-    print ("erreur")
-    exit()
-
-if minute < 0 or minute > 59:
-    print ("erreur")
-    exit()
-
-
-
-if heure < 1:
-   print ((str(heure+12))+(arg[point])+(str(bloc2))+"AM")
-   exit()
-if heure == 12:
-   print((str(heure))+(arg[point])+(str(bloc2))+"PM")
-   exit()
-
-if heure < 12:
-   print((str(heure))+(arg[point])+(str(bloc2))+"AM")
-   exit()
-else:
-   if heure > 12:
-      print ((str(heure-12))+(arg[point])+(str(bloc2))+"PM")
+def argument_format():
+   '''Check if argument is in the correct format.'''
+   if len(sys.argv[1:]) != 1:
+      print("erreur")
       exit()
+
+   search_ellipsis = sys.argv[1].find(":")
+   if search_ellipsis == -1:
+      print ("erreur")
+      exit()
+
+   for argument in sys.argv[1:]:
+      ellipsis_index = argument.find(":")
+
+   bloc_hours = argument[:(ellipsis_index)]
+   bloc_minutes = argument[((ellipsis_index)+1):]
+
+   if len(bloc_hours) != 2 and len(bloc_minutes) != 2:
+      print("erreur")
+      exit()
+   check_is_figure(bloc_hours, bloc_minutes,argument, ellipsis_index)
+
+
+def check_is_figure(bloc_hours, bloc_minutes,argument, ellipsis_index):
+   bloc_hours_is_figure=bloc_hours.isdigit()
+   bloc_minutes_is_figure=bloc_minutes.isdigit()
+
+   if not bloc_hours_is_figure or not bloc_minutes_is_figure:
+      print("erreur")
+      exit()
+   schedule(bloc_hours, bloc_minutes,argument, ellipsis_index)
+
+
+def schedule(bloc_hours, bloc_minutes, argument, ellipsis_index):
+   hours = int(bloc_hours)
+   minutes = int(bloc_minutes)
+   if hours < 0 or hours > 23:
+      print ("erreur")
+      exit()
+   if minutes < 0 or minutes > 59:
+      print ("erreur")
+      exit()
+   if hours < 1:
+      print ((str(hours+12))+(argument[ellipsis_index])+(str(bloc_minutes))+"AM")
+      exit()
+   if hours == 12:
+      print((str(hours))+(argument[ellipsis_index])+(str(bloc_minutes))+"PM")
+      exit()
+   if hours < 12:
+      print((str(hours))+(argument[ellipsis_index])+(str(bloc_minutes))+"AM")
+      exit()
+   else:
+      if hours > 12:
+         print ((str(hours-12))+(argument[ellipsis_index])+(str(bloc_minutes))+"PM")
+         exit()
+
+argument_format()

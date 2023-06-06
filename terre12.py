6bb205f248bc4215d10a898a46d5ea1813f7e214
JackFloGit <jack.flor@hotmail.com>
2023-06-06 15:23:37 +0200

    terre12

--- terre12.py
+++ terre12.py
@@ -1,104 +1,89 @@
-# Créez un programme qui transforme une heure 
-# affichée en format12h en une heure affichée 
-# au format 24h.
-#
-# exp :
-#       -->python script.py 11:40PM
-#          23:40
-# !! attention à midi et minuit !!
-#
+'''Créez un programme qui transforme une heure 
+ affichée en format12h en une heure affichée 
+ au format 24h.
+
+ exp :
+       -->python script.py 11:40PM
+          23:40
+ !! attention à midi et minuit !!'''
+
 
 import sys
 
-#format/":"
-if len(sys.argv[1:]) != 1 :
-    print("erreur")
-    exit()
-found=sys.argv[1].find(":")
-if found == -1:
-    print ("erreur")
-    exit()
+
+def argurment_format():
+    if len(sys.argv[1:]) != 1:
+        print("erreur")
+        exit()
+    search_ellipsis = sys.argv[1].find(":")
+    if search_ellipsis == -1:
+        print ("erreur")
+        exit()
 
 
-    
-for arg in sys.argv[1:]:
+def check_suffix():
+    for argument in sys.argv[1:]:
+        ellipsis_index = argument.find(":")
+        search_M_ = argument.find("M")
+        if search_M_ == -1:
+            print("erreur")
+            exit()
+        bloc_hours = argument[:(ellipsis_index)]
+        bloc_minutes = argument[(ellipsis_index)+1:(search_M_)-1]
+        am_pm = argument[(search_M_)-1:(search_M_)+1]
+        suffix_is_letter = am_pm.isalpha()
+        if not suffix_is_letter:
+            print("erreur")
+            exit()
+        if am_pm != "AM" and am_pm != "PM":
+            print("erreur")
+            exit()
+        check_is_figure(bloc_hours, bloc_minutes, am_pm, argument, ellipsis_index)
+
+def check_is_figure(bloc_hours, bloc_minutes, am_pm, argument, ellipsis_index):
+    if len(bloc_hours) < 1 or len(bloc_hours) > 2:
+        print ("erreur")
+        exit()
+    if len(bloc_minutes) != 2:
+        print ("erreur")
+        exit()
+    bloc_hours_is_figure = bloc_hours.isdigit()
+    bloc_minutes_is_figure = bloc_minutes.isdigit()
+    if not bloc_hours or not bloc_minutes:
+        print("erreur")
+        exit()
+    schedule_24H(bloc_hours, bloc_minutes, am_pm, argument, ellipsis_index)
 
 
-#lettre
-
- point=arg.find(":")
-ltr=arg.find("M")
-
-if ltr == -1:
-    print("erreur")
-    exit()
-
-bloc1=arg[:(point)]
-bloc2=arg[(point)+1:(ltr)-1]
-ampm=arg[(ltr)-1:(ltr)+1]
-
-alpha= ampm.isalpha()
-
-if alpha is False:
-    print("erreur")
-    exit()
-
-if ampm != "AM" and ampm != "PM":
-    print("erreur")
-    exit()
-
-#chiffre
-
-if len(bloc1) < 1 or len(bloc1) > 2:
-    print ("erreur")
-    exit()
-
-if len(bloc2) !=2:
-    print ("erreur")
-    exit()
-
-isdig1=bloc1.isdigit()
-isdig2=bloc2.isdigit()
-
-if isdig1 != True or isdig2 != True:
-    print("erreur")
-    exit()
-
-
-#syst24h
-
-
-heure=int(bloc1)
-minute=int(bloc2)
-
-
-if heure < 1 or heure > 12:
-    print ("erreur")
-    exit()
-
-
-if minute < 0 or minute > 59:
-    print ("erreur")
-    exit()
-
-
-if ampm != "PM":
-    if heure != 12:
-        if heure < 10:
-         print ('0'+bloc1+':'+bloc2)
-         exit()
+def schedule_24H(bloc_hours, bloc_minutes, am_pm, argument, ellipsis_index):
+    hours = int(bloc_hours)
+    minutes = int(bloc_minutes)
+    if hours < 1 or hours > 12:
+        print ("erreur")
+        exit()
+    if minutes < 0 or minutes > 59:
+        print ("erreur")
+        exit()
+    if am_pm != "PM":
+        if hours != 12:
+            if hours < 10:
+             print ('0' + bloc_hours + argument[ellipsis_index] + bloc_minutes)
+             exit()
+            else:
+                print (bloc_hours + argument[ellipsis_index] + bloc_minutes)
+                exit()
         else:
-            print (bloc1+':'+bloc2)
+            morning = hours-12
+            print('0'+ str(morning) + argument[ellipsis_index] + bloc_minutes)
             exit()
     else:
-        matin=heure-12
-        print('0'+str(matin)+':'+bloc2)
-        exit()
-else:
-    if heure != 12:
-        Après_midi=heure+12
-        print(str(Après_midi)+':'+bloc2)
-        exit()
-    else:
-        print(str(heure)+':'+bloc2)
-        exit()
+        if hours != 12:
+            afternoon = hours+12
+            print(str(afternoon) + argument[ellipsis_index] + bloc_minutes)
+            exit()
+        else:
+            print(str(hours) + argument[ellipsis_index] + bloc_minutes)
+            exit()
+
+argurment_format()
+check_suffix()

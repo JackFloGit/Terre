08b67f8a6825ecf0158d722ef99c92304c5ceffe
JackFloGit <jack.flor@hotmail.com>
2023-06-06 16:42:17 +0200

    terre13

--- terre13.py
+++ terre13.py
@@ -1,62 +1,54 @@
-# Créez un programme qui prend en paramètre trois entiers 
-# et affiche la valeur du milieu.
-#
-#   exp :
-#           --> python script.py 11 40 34
-#               34
-#           --> python script.py 2 1 3
-#               2
-#           --> python script.py 2 2 2
-#               erreur.
-#
+'''Créez un programme qui prend en paramètre trois entiers
+et affiche la valeur du milieu.
+
+   exp :
+           --> python script.py 11 40 34
+               34
+           --> python script.py 2 1 3
+               2
+           --> python script.py 2 2 2
+               erreur.'''
+
 
 import sys
 
-if len(sys.argv[1:]) != 3:
-    print("erreur")
-    exit() 
-    
-isdig0=(sys.argv[1]).isdigit()
-isdig1=(sys.argv[2]).isdigit()
-isdig2=(sys.argv[3]).isdigit()
 
-if isdig0 is False:
-    print("erreur")
-    exit()
+def lenght_argv():
+    if len(sys.argv[1:]) != 3:
+        print("erreur")
+        exit() 
 
-if isdig1 is False:
-    print("erreur")
-    exit() 
 
-if isdig2 is False:
-    print("erreur")
-    exit()
-int0=int(sys.argv[1])
-int1=int(sys.argv[2])
-int2=int(sys.argv[3])
+def format_each_arguments():
+    first_argument = (sys.argv[1]).isdigit()
+    second_argument = (sys.argv[2]).isdigit()
+    third_argument = (sys.argv[3]).isdigit()
+    if not first_argument or not second_argument or not third_argument:
+        print("erreur")
+        exit()
+    first_integer = int(sys.argv[1])
+    second_integer = int(sys.argv[2])
+    third_integer = int(sys.argv[3])
+    sorting_rules(first_integer, second_integer, third_integer)
 
-if int0 == int1:
-    print("erreur")
-    exit()
-if int0 == int2:
-    print("erreur")
-    exit()
-if int1 == int2:
-    print("erreur")
-    exit()
 
-Arg=[]
-Arg.append(int0)
+def sorting_rules(first_integer, second_integer, third_integer):
+    if first_integer == second_integer or first_integer == third_integer or second_integer == third_integer:
+        print("erreur")
+        exit()
+    middle_list=[]
+    middle_list.append(first_integer)
+    if (second_integer) < (middle_list[0]):
+        middle_list.insert(0,first_integer)
+    else:
+        middle_list.append(second_integer)
+    if (third_integer) < (middle_list[0]):
+        middle_list.insert(0,third_integer)
+    elif (third_integer) < (middle_list[1]):
+        middle_list.insert(1,third_integer)
+    else:
+        middle_list.append(third_integer)
+    print(middle_list[1])
 
-if (int1) < (Arg[0]):
-    Arg.insert(0,int1)
-else:
-    Arg.append(int1)
-if (int2) < (Arg[0]):
-    Arg.insert(0,int2)
-elif (int2) < (Arg[1]):
-    Arg.insert(1,int2)
-else:
-    Arg.append(int2)
-
-print(Arg[1])
+lenght_argv()
+format_each_arguments()

23df07c868602a38c375f85d2e2048b2f1b90872
JackFloGit <jack.flor@hotmail.com>
2023-06-06 17:43:09 +0200

    terre14

--- terre14.py
+++ terre14.py
@@ -1,45 +1,46 @@
-# Créez un programme qui détermine si une liste d’entiers est triée ou pas.
-#
-#
-# Exp :
-#       --> python script.py 9 8 3
-#           Pas triée !
-#
-#       --> python script.py 3 8 9 12
-#           Triée !
-#
-#       --> python script.py “Salut”
-#           erreur.
-#
+'''Créez un programme qui détermine si une liste d’entiers est triée ou pas.
+
+
+ Exp :
+       --> python script.py 9 8 3
+           Pas triée !
+
+       --> python script.py 3 8 9 12
+           Triée !
+
+       --> python script.py “Salut”
+           erreur.'''
+
 
 import sys
 
-if len(sys.argv[1:]) < 2:
-    print("erreur")
-    exit()
 
-list0= []
-list1= []
-
-for arg in sys.argv[1:]:
-    if not arg.isdigit():
+def lenght_argv():
+    if len(sys.argv[1:]) < 2:
         print("erreur")
         exit()
-    intarg=int(arg)
-    list0.append(intarg)
-    list1.append(intarg)
 
-list2=[]
+def argument_is_integer_in_list():
+    original_list = []
+    original_list_copied = []
+    for argument in sys.argv[1:]:
+        if not argument.isdigit():
+            print("erreur")
+            exit()
+        argument_integer = int(argument)
+        original_list.append(argument_integer)
+        original_list_copied.append(argument_integer)
+    sorting_process(original_list, original_list_copied)
 
-while len(list1) != 0:
-    i=list1.index(min(list1))
-    list2.append(list1[i])
-    list1.pop(i)
 
-if list0 != list2:
-    print("Pas triée !")
+def sorting_process(original_list, original_list_copied):
+    comparison_list = []
+    while len(original_list_copied) != 0:
+        index_sort = original_list_copied.index(min(original_list_copied))
+        comparison_list.append(original_list_copied[index_sort])
+        original_list_copied.pop(index_sort)
+    print("Pas triée !") if original_list != comparison_list else print("Triée !")
     exit()
-else:
-    print("Triée !")
-    exit()
 
+lenght_argv()
+argument_is_integer_in_list()
